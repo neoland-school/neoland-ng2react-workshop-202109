537cb2858246187fd74c4517ae220915c8dc8b42
efarriolsalas <efarriol@gmail.com>
2021-09-07 20:41:43 +0200

    add code

--- staff/elisabet-farriol/my-app/src/Register.js
+++ staff/elisabet-farriol/my-app/src/Register.js
@@ -1,5 +1,45 @@
-function Register() {
-    return <h1>TODO mount me!</h1>
+import { register } from './user.service'
+import { Component } from 'react'
+import './Register.css'
+
+class Register extends Component {
+    constructor() {
+        super()
+
+        this.state = { error: null }
+
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+
+    handleSubmit(event) {
+        event.preventDefault()
+
+        const { target: { name: {value: name}, email: { value: email }, password: { value: password } } } = event
+
+        try {
+            register(name, email, password)
+                .then(token => {
+                    sessionStorage.token = token
+
+                    this.props.onLogin()
+                })
+                .catch(({ message }) => {
+                    this.setState({ error: message })
+                })
+        } catch ({message}) {
+            this.setState({ error: message })
+        }
+    }
+
+    render() {
+        return <form className="register" onSubmit={this.handleSubmit}>
+            {this.state.error && <p>{this.state.error}</p>}
+            <input type="name" name="name" placeholder="name" />
+            <input type="email" name="email" placeholder="e-mail" />
+            <input type="password" name="password" placeholder="password" />
+            <button>Login</button>
+        </form>
+    }
 }
 
 export default Register
